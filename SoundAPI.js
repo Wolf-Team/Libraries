887d38ee300a609825efaf18a974f9df00710cf2
80LK <80LK@users.noreply.github.com>
2018-06-26 22:54:37 +0300

    Update SoundAPI to version 2
    Update SoundAPI to version 2
    -Added class MultiSound
    -Added setInEntity and setVolume methods for the Sound class

--- SoundAPI.js
+++ SoundAPI.js
@@ -7,11 +7,18 @@
                                                                 
     SoundAPI library
 
+    Внимание! Запрещено:
+    1.Распространение библиотеки на сторонних источниках без указание ссылки на официальное сообщество
+    2.Изменение кода
+    3.Явное копирование кода
+
+    Используя библиотеку вы автоматически соглашаетесь с этими правилами.
+
     ©WolfTeam ( https://vk.com/wolf___team )
  */
 LIBRARY({
     name: "SoundAPI",
-    version: 1,
+    version: 2,
     shared: true,
     api: "CoreEngine",
 });
@@ -19,17 +26,34 @@ LIBRARY({
 var _soundUtils = {
 	sounds:[],
 	
+	source:{
+		PLAYER:1,
+		BLOCK:2,
+		ENTITY:3,
+	},
+	
 	updateVolume:function(){
 		for(var i in this.sounds){
 			var s = this.sounds[i];
-			if(s.inPlayer)continue;
+			if(s.source==this.source.PLAYER)continue;
 			
 			var pp = Player.getPosition();//x,y,z
+			if(s.source==this.source.BLOCK){
+				var pb = {
+						x:s.position.x-pp.x,
+						y:s.position.y-pp.y,
+						z:s.position.z-pp.z
+					};
+			}else if(s.source==this.source.ENTITY){
+				var pe = Entity.getPosition(s.entity);
+				var pb = {
+						x:pe.x-pp.x,
+						y:pe.y-pp.y,
+						z:pe.z-pp.z
+					};
+			}
 			
-			var pb = {
-				x:s.position.x-pp.x,
-				y:s.position.y-pp.y,
-				z:s.position.z-pp.z };
+		
 			var pz = { x:0, y:0, z:5 };
 			
 			var pAngle = ((2*Math.PI) - (Entity.getLookAngle(Player.get()).yaw%(2*Math.PI)))%(2*Math.PI);
@@ -53,6 +77,24 @@ var _soundUtils = {
 				s.media.setVolume((0.1 + (radVol*angle))*s.volume/(s.radius - 2)*_distance, (1 - (radVol*angle))*s.volume/(s.radius - 2)*_distance);
 			
 		}
+	},
+	
+	leaveWorld:function(){
+		for(var i in this.sounds){
+			var s = this.sounds[i];
+			if(s.source==this.source.PLAYER)continue;
+			
+			s.pause();
+		}
+	},
+	
+	loadedWorld:function(){
+		for(var i in this.sounds){
+			var s = this.sounds[i];
+			if(s.source==this.source.PLAYER)continue;
+			
+			s.play();
+		}
 	}
 };
 
@@ -63,13 +105,28 @@ var Sound = function(src, vol){
 	this.media.setDataSource(this.path);
 	this.media.prepare();
 	
-	this.inPlayer = true;
+	this.source = _soundUtils.source.PLAYER;
 	this.position = {x:0, y:0, z:0};
+	this.entity = Player.get();
 	this.radius = 5;
 	this.volume = vol||1;
+	this.media.setVolume(this.volume, this.volume);
+	
+	this.setVolume=function(vol){
+		this.volume = vol;
+		
+		if(this.source == _soundUtils.source.PLAYER)
+			this.media.setVolume(this.volume, this.volume);
+	}
+	
+	this.setInEntity = function(ent, r){
+		this.source = _soundUtils.source.ENTITY;
+		this.entity = ent;
+		this.radius = r;
+	}
 	
 	this.setInBlock = function(vector, y, z, r){
-		this.inPlayer = false;
+		this.source = _soundUtils.source.BLOCK;
 		
 		if(typeof(vector)=='number'){
 			this.position = {
@@ -88,7 +145,7 @@ var Sound = function(src, vol){
 		
 	}
 	this.setInPlayer = function(){
-		this.inPlayer = true;
+		this.source = _soundUtils.source.PLAYER;
 	}
 	
 	this.setOnCompletion = function(f){
@@ -101,7 +158,7 @@ var Sound = function(src, vol){
 	
 	this.setSource = function(src){
 		this.reset();
-		this.path = __dir__+src;
+		this.path = __dir__+"sounds/"+src;
 		this.media.setDataSource(this.path);
 		this.media.prepare(); 
 	};
@@ -130,8 +187,78 @@ var Sound = function(src, vol){
 	this._id = _soundUtils.sounds.push(this);
 }
 
+var MultiSound = function(params){
+	this.components = [];
+	for(var i = 0; i < params.length; i++){
+		this.components.push(new Sound(params[i].src, params[i].volume||1));
+	}
+	
+	this.setInEntity = function(ent, r){
+		for(var i = 0; i < this.components.length; i++){
+			this.components[i].setInEntity(ent, r);
+		}
+	}
+	
+	this.setInBlock = function(vector, y, z, r){
+		for(var i = 0; i < this.components.length; i++){
+			this.components[i].setInBlock(vector, y, z, r);
+		}
+	}
+	this.setInPlayer = function(){
+		for(var i = 0; i < this.components.length; i++){
+			this.components[i].setInPlayer();
+		}
+	}
+	
+	
+	this.addVolume = function(vol){
+		for(var i = 0; i < this.components.length; i++){
+			this.components[i].setVolume(this.components[i].volume+vol);
+		}
+	}
+	
+	this.setLooping = function(a){
+		for(var i = 0; i < this.components.length; i++){
+			this.components[i].setLooping(a);
+		}
+	}
+	
+	this.play = function(){
+		for(var i = 0; i < this.components.length; i++){
+			this.components[i].play();
+		}
+	};
+	
+	this.pause = function(){
+		for(var i = 0; i < this.components.length; i++){
+			this.components[i].pause();
+		}
+	};
+	
+	this.reset = function(){
+		for(var i = 0; i < this.components.length; i++){
+			this.components[i].reset();
+		}
+	};
+	
+	this.stop = function(){
+		for(var i = 0; i < this.components.length; i++){
+			this.components[i].stop();
+		}
+	};
+	
+	
+}
+
 Callback.addCallback("tick", function () {
 	_soundUtils.updateVolume();
 });
+Callback.addCallback("LevelLoaded", function () {
+	_soundUtils.loadedWorld();
+});
+Callback.addCallback("LevelLeft", function () {
+	_soundUtils.leaveWorld();
+});
 
 EXPORT("Sound", Sound);
+EXPORT("MultiSound", MultiSound);
