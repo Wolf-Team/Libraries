c1566bcc305f66cfa58dd1588cf907ecde959f6b
80LK <80LK@users.noreply.github.com>
2020-02-20 18:49:31 +0300

    Update StructuresAPI
    - Object StructuresAPI removed.
    - Added Rotate object. Used for complex turns.
    - The get structure method has been changed. struct.get (x, y, z, rotates, return_index).
    - The set structure method has been changed. struct.set (x, y, z, rotate, progressively, time).
    - Added destroy (x, y, z, rotates, progressively, time) method for structure.
    - Added check (...) method for structure. Equivalent to get (...) method.
    - Added method Structure.setInWorld (name, ...). Alternative to Structure.get (name) .set (...).
    - Added method Structure.destroyInWorld (name, ...). Alternative to Structure.get (name) .destroy (...).
    - Added constants Structure.PROGRESSIVELY and Structure.NOT_PROGRESSIVELY.
    - Added constants Structure.MIRROR_X, Structure.MIRROR_Y and Structure.MIRROR_Z.
    - Fixed installation of blocks added by the mod.
    - Fixed saving items and blocks.
    - Fixed rotation of 180 degrees in Y.
    - Saved TileEntity

--- StructuresAPI.js
+++ StructuresAPI.js
@@ -18,6 +18,21 @@
     ©WolfTeam ( https://vk.com/wolf___team )
 */
 /*  ChangeLog:
+	v1.3
+	- StructuresAPI удален.
+	- Добавлен объект Rotate. Используется для сложных поворотов.
+	- Метод структуры get был изменен. struct.get(x, y, z, rotates, return_index).
+	- Метод структуры set был изменен. struct.set(x, y, z, rotate, progressively, time).
+	- Добавлен метод destroy(x, y, z, rotates, progressively, time) для структуры.
+	- Добавлен метод check(...) для структуры. Эквивалентен методу get(...).
+	- Добавлен метод Structure.setInWorld(name, ...). Альтернатива Structure.get(name).set(...).
+	- Добавлен метод Structure.destroyInWorld(name, ...). Альтернатива Structure.get(name).destroy(...).
+	- Добавлены константы Structure.PROGRESSIVELY и Structure.NOT_PROGRESSIVELY.
+	- Добавлены константы Structure.MIRROR_X, Structure.MIRROR_Y и Structure.MIRROR_Z.
+	- Исправлена установка блоков добавленных модом.
+	- Исправлено сохранение предметов и блоков.
+	- Исправлен поворот на 180 градусов по Y.
+	- Сохраняются TileEntity
 	v1.2
 	- Библиотека переписана. Объект StructuresAPI устарел.
 	- Сохраняется содержимое сундуков, печей и воронок
@@ -31,14 +46,15 @@
 
 LIBRARY({
     name: "StructuresAPI",
-    version: 3,
+    version: 4,
     shared: false,
     api: "CoreEngine"
 });
 
 var StructuresDB = {
 	structures:{},
-	dir:"structures"
+	dir:"structures",
+	versionSaver:2
 }
 
 var Structure = function(name, alerted){
@@ -47,38 +63,12 @@ var Structure = function(name, alerted){
 		return name;
 	}
 	
+	if(!StructuresDB.structures.hasOwnProperty(name))
+		StructuresDB.structures[name] = this;
+	
 	var structure = [];
 	var chests = {};
-	
-	/* READ STRUCTURE */
-	if(!StructuresDB.structures.hasOwnProperty(name)){
-		StructuresDB.structures[name] = this;
-
-		var path = __dir__ + StructuresDB.dir + "/" + name + ".struct";
-
-		if(FileTools.isExists(path)){
-			read = JSON.parse(FileTools.ReadText(path));
-			if(read){
-				if(read.version){
-					switch(read.version){
-						case 1:
-							structure = read.struture;
-							if(read.chests)
-								chests = read.chests;
-
-						break;
-						default:
-							Translation.sprintf("Unknown version \"%s\".", read.version);
-						break;
-					}
-				}else{
-					structure = read;
-				}
-			}
-		}else{
-			if(alerted) alert(Translation.sprintf("Structure \"%s\" not found.", name));
-		}	
-	}
+	var TileEntitys = {};
 	
 	this.clear = function(){
 		structure = [];
@@ -107,7 +97,7 @@ var Structure = function(name, alerted){
 			let slot = te.getSlot(i);
 			if(slot && slot.id != 0 && slot.count > 0){
 				let item = {
-					id:slot.id,
+					id: getSID(slot.id),
 					data:slot.data,
 					count:slot.count
 				};
@@ -118,15 +108,36 @@ var Structure = function(name, alerted){
 		chests[name] = chest;
 	}
 	
+	this.addTileEntity = function(name, te){
+		TileEntitys[name] = {
+			data:te.data
+		};
+		
+		let slots = te.container.slots;
+		let _slots = {};
+		for(let i in slots){
+			if(slots[i].id){
+				_slots[i] = {
+					id:getSID(slots[i].id),
+					count:slots[i].count,
+					data:slots[i].data
+				};
+			}
+		}
+		
+		if(Object.keys(_slots).length)
+			TileEntitys[name].slots = _slots;
+	}
+	
 	this.save = function(){
 		if(!FileTools.isExists(__dir__ + StructuresDB.dir))
 			FileTools.mkdir(__dir__ + StructuresDB.dir);
 		
 		for(var i = 0; i < structure.length; i++){
 			if(structure[i][3] && !structure[i][3].id){
-				structure[i][3] = getBlockSID(structure[i][3]);
+				structure[i][3] = getSID(structure[i][3]);
 			}else{
-				structure[i][3].id = getBlockSID(structure[i][3].id);
+				structure[i][3].id = getSID(structure[i][3].id);
 			
 				if(structure[i][3].data == 0)
 					structure[i][3] = structure[i][3].id;
@@ -134,33 +145,59 @@ var Structure = function(name, alerted){
 		}
 		
 		let saveObject = {
-			version:1,
-			struture:structure
+			version:StructuresDB.versionSaver,
+			structure:structure
 		};
 
 		if(Object.keys(chests).length){
 			for(let i in chests){
 				for(let j = 0, l = chests[i].length; j < l; j++){
-					chests[i][j][1].id = getBlockSID(chests[i][j][1].id);
+					chests[i][j][1].id = getSID(chests[i][j][1].id);
 				}
 			}
 			saveObject.chests = chests;
 		}
+		
+		if(Object.keys(TileEntitys).length)
+			saveObject.te = TileEntitys;
 
 		FileTools.WriteText(__dir__ + StructuresDB.dir + "/" + name + ".struct", JSON.stringify(saveObject));
 	}
 	
-	this.get = function(x, y, z, rotate_y){
-		if(rotate_y === undefined) rotate_y = false;
-		var rotates = rotate_y == true ? Structure.ROTATE_Y : Structure.ROTATE_ALL;
+	this.get = function(x, y, z, rotates, return_index){
+		if(rotates == undefined) rotates = [Structure.ROTATE_NONE];
+		if(rotates === true) rotates = Structure.ROTATE_Y;
+		
+		if(rotates instanceof Array && !(rotates[0] instanceof Array || rotates[0] instanceof Rotate)){
+			rotates = [rotates];
+		}
+		
 		
 		for(var i = 0; i < rotates.length; i++){
+			
 			var rotate = rotates[i];
+			
+			if(rotate instanceof Array && !(rotate[0] instanceof Array))
+				rotate = [rotate];
+				
+			if(rotate instanceof Rotate)
+				rotate = rotate.get();
+			
 			for(var k = 0; k < structure.length; k++){
-				var block = structure[k], id, data =0;
-				var dx = block[0] * rotate[0] + block[1] * rotate[1] + block[2] * rotate[2];
-				var dy = block[0] * rotate[3] + block[1] * rotate[4] + block[2] * rotate[5];
-				var dz = block[0] * rotate[6] + block[1] * rotate[7] + block[2] * rotate[8];
+				var block = structure[k], id, data = 0;
+				
+				var dx = block[0];
+				var dy = block[1];
+				var dz = block[2];
+				
+				for(let j = 0, l = rotate.length; j < l; j++){
+					let _dx = dx * rotate[j][0] + dy * rotate[j][1] + dz * rotate[j][2];
+					let _dy = dx * rotate[j][3] + dy * rotate[j][4] + dz * rotate[j][5];
+					let _dz = dx * rotate[j][6] + dy * rotate[j][7] + dz * rotate[j][8];
+					dx = _dx;
+					dy = _dy;
+					dz = _dz;
+				}
 				
 				if(typeof block[3] == "number")
 					id = block[3];
@@ -174,62 +211,214 @@ var Structure = function(name, alerted){
 				_block = World.getBlock(x + dx, y+dy, z+dz);
 				if(_block.id != id || _block.data != data) break;
 				
-				if(_block.id == id && _block.data == data && k == structure.length-1) return true;
+				if(_block.id == id && _block.data == data && k == structure.length-1)
+					return return_index ? i : true;
 				
 			}
 		}
 		
-		return false;
+		return return_index ? -1 : false;
 	}
-
-	this.set = function (x, y, z, rotate, destroy, progressively, time) {
+	
+	this.check = this.get;
+	
+	this.set = function(x, y, z, rotate, progressively, time){
 		if (rotate === undefined) rotate = Structure.ROTATE_NONE;
-		if (destroy === undefined) destroy = false;
-		if (progressively === undefined) progressively = false;
+		if (progressively === undefined) progressively = Structure.NOT_PROGRESSIVELY;
 		if (time === undefined) time = 0;
-
-		if (rotate[0] instanceof Array) {
-			if (rotate.indexOf(Structure.ROTATE_NONE) == -1)
-				rotate.push(Structure.ROTATE_NONE);
-
+		
+		if(rotate instanceof Array && (rotate[0] instanceof Array || rotate[0] instanceof Rotate))
 			rotate = rotate[Math.round(rand(0, rotate.length - 1))];
-		}
-
+		
+		if(rotate instanceof Rotate)
+			rotate = rotate.get();
+		else
+			rotate = [rotate];
+		
 		new java.lang.Thread(function () {
 			for (var i = 0; i < structure.length; i++) {
 				var block = structure[i];
 				var id, data = 0;
 
-				if (typeof block[3] == "number") {
+				if(typeof block[3] == "number")
 					id = block[3];
-				} else {
-					id = block[3].id || 2;
-					data = block[3].data || block[3].meta || 0;
+				else if(typeof block[3] == "string")
+					id = BlockID[block[3]];
+				else{
+					id = block[3].id || 1;
+					data = block[3].data;
 				}
-
-				var dx = block[0] * rotate[0] + block[1] * rotate[1] + block[2] * rotate[2];
-				var dy = block[0] * rotate[3] + block[1] * rotate[4] + block[2] * rotate[5];
-				var dz = block[0] * rotate[6] + block[1] * rotate[7] + block[2] * rotate[8];
-
-				if (!destroy){
-					World.setBlock(x + dx, y + dy, z + dz, id, data);
-					if(block[4]){
+				
+				var dx = block[0];
+				var dy = block[1];
+				var dz = block[2];
+				
+				for(var j = 0, l = rotate.length; j < l; j++){
+					let _dx = dx * rotate[j][0] + dy * rotate[j][1] + dz * rotate[j][2];
+					let _dy = dx * rotate[j][3] + dy * rotate[j][4] + dz * rotate[j][5];
+					let _dz = dx * rotate[j][6] + dy * rotate[j][7] + dz * rotate[j][8];
+					dx = _dx;
+					dy = _dy;
+					dz = _dz;
+				}
+				
+				World.setBlock(x + dx, y + dy, z + dz, id, data);
+				if(block[4]){
+					if([54, 61, 62, 154].indexOf(id) != -1){
 						let TE = World.getContainer(x + dx, y + dy, z + dz);
 
 						let chest = chests[block[4]];
-						for(let i = 0, l = chest.length; i < l; i++){
-							TE.setSlot(chest[i][0], chest[i][1].id, chest[i][1].count, chest[i][1].data || 0);
+						for(let j = 0, l = chest.length; j < l; j++){
+							let item_id = chest[j][1].id;
+							
+							if(isNaN(parseInt(item_id)))
+								item_id = BlockID[item_id] || ItemID[item_id];
+							
+							TE.setSlot(chest[j][0], item_id, chest[j][1].count, chest[j][1].data || 0);
+						}
+					}else{
+						let te_storage = TileEntitys[block[4]];
+						let TE = World.addTileEntity(x + dx, y + dy, z + dz);
+						TE.data = te_storage.data;
+						
+						if(te_storage.slots){
+							for(let j in te_storage.slots){
+								let slot = te_storage.slots[j];
+								let item_id = slot.id;
+								
+								if(isNaN(parseInt(item_id)))
+									item_id = BlockID[item_id] || ItemID[item_id];
+								
+								TE.container.setSlot(j, item_id, slot.count, slot.data);
+							}
 						}
 					}
-				}else{
-					World.setBlock(x + dx, y + dy, z + dz, 0, 0)
 				}
 
 				if(progressively)java.lang.Thread.sleep(time || 100);
 			}
 		}).start();
 	}
+	
+	this.destroy = function(x,y,z, rotates, progressively, time){
+		if (rotates === undefined) rotates = Structure.ROTATE_NONE;
+		if (progressively === undefined) progressively = Structure.NOT_PROGRESSIVELY;
+		if (time === undefined) time = 100;
+		
+		let index = this.get(x, y, z, rotates, true);
+		if(index != -1){
+			let rotate = rotates[index];
+			
+			if(rotate instanceof Array && !(rotate[0] instanceof Array))
+				rotate = [rotate];
+				
+			if(rotate instanceof Rotate)
+				rotate = rotate.get();
+			
+			new java.lang.Thread(function () {
+				for (var i = 0; i < structure.length; i++) {
+					var block = structure[i];
+					
+					var dx = block[0];
+					var dy = block[1];
+					var dz = block[2];
+					
+					for(var j = 0, l = rotate.length; j < l; j++){
+						let _dx = dx * rotate[j][0] + dy * rotate[j][1] + dz * rotate[j][2];
+						let _dy = dx * rotate[j][3] + dy * rotate[j][4] + dz * rotate[j][5];
+						let _dz = dx * rotate[j][6] + dy * rotate[j][7] + dz * rotate[j][8];
+						dx = _dx;
+						dy = _dy;
+						dz = _dz;
+					}
+					
+					World.setBlock(x + dx, y + dy, z + dz, 0);
+
+					if(progressively)
+						java.lang.Thread.sleep(time);
+				}
+			}).start();
+		
+		}
+	}
+
+	/* READ STRUCTURE */
+	var path = __dir__ + StructuresDB.dir + "/" + name + ".struct";
+
+	if(FileTools.isExists(path)){
+		read = JSON.parse(FileTools.ReadText(path));
+		if(read){
+			let version = 0;
+			if(read.version){
+				version = read.version;
+				switch(read.version){
+					case 2:
+						structure = read.structure;
+						if(read.chests) chests = read.chests;
+						if(read.te) TileEntitys = read.te;
+					break;
+					case 1:
+						structure = read.struture;
+						if(read.chests) chests = read.chests;
+					break;
+					default:
+						Translation.sprintf("Unknown version \"%s\".", read.version);
+					break;
+				}
+			}else{
+				structure = read;
+			}
+			
+			if(version!=StructuresDB.version)
+				this.save();
+		}
+	}else{
+		if(alerted) alert(Translation.sprintf("Structure \"%s\" not found.", name));
+	}	
 };
+
+var Rotate = function(r){
+	var rotates = [];
+	
+	this.addRotate = function(matrix){
+		if(!matrix instanceof Array && !matrix instanceof Rotate)
+			throw "is not matrix";
+		
+		if(matrix instanceof Rotate)
+			return this.addRotates(matrix.get());
+		
+		if(matrix.length != 9) throw "Not 9 number";
+		
+		for(let i = 0; i < 9; i++)
+			if(Math.abs(matrix[i]) > 1)
+				throw "Not normal matrix";
+		
+		rotates.push(matrix);
+	}
+	
+	this.add = this.addRotate;
+	
+	this.addRotates = function(rotates){
+		if(!rotates instanceof Array) throw "is not array";
+		
+		for(let i = 0; i < rotates.length; i++)
+			this.addRotate(rotates[i]);
+	}
+	
+	this.adds = this.addRotates;
+	
+	this.get = function(){
+		return rotates;
+	}
+
+	if(r){
+		if(r instanceof Array && (r[0] instanceof Array || r[0] instanceof Rotate)){
+			this.addRotates(r);
+		}else{
+			this.addRotate(r);
+		}
+	}
+}
 
 /* ROTATE CONSTANTS */
 Structure.ROTATE_NONE = [
@@ -239,19 +428,19 @@ Structure.ROTATE_NONE = [
 ];
 	
 Structure.ROTATE_90Y = [
-	0,0,1,
-	0,1,0,
-	-1,0,0
-];
-Structure.ROTATE_180Y = [
-	0,0,1,
-	0,1,0,
-	1,0,0
-];
-Structure.ROTATE_270Y = [
 	0,0,-1,
 	0,1,0,
 	1,0,0
+];
+Structure.ROTATE_180Y = [
+	-1,0,0,
+	0,1,0,
+	0,0,-1
+];
+Structure.ROTATE_270Y = [
+	0,0,1,
+	0,1,0,
+	-1,0,0
 ];
 	
 Structure.ROTATE_90X = [
@@ -300,11 +489,30 @@ Structure.ROTATE_RANDOM = [
 ];
 Structure.ROTATE_ALL = Structure.ROTATE_RANDOM;
 Structure.ROTATE_Y = [
+	Structure.ROTATE_NONE,
 	Structure.ROTATE_90Y,
-	Structure.ROTATE_270Y,
 	Structure.ROTATE_180Y,
-	Structure.ROTATE_NONE
+	Structure.ROTATE_270Y
 ];
+
+Structure.MIRROR_X = [
+	-1,0,0,
+	0,1,0,
+	0,0,1
+];
+Structure.MIRROR_Y = [
+	1,0,0,
+	0,-1,0,
+	0,0,1
+];
+Structure.MIRROR_Z = [
+	1,0,0,
+	0,1,0,
+	0,0,-1
+];
+
+Structure.PROGRESSIVELY = true;
+Structure.NOT_PROGRESSIVELY = false;
 
 Structure.get = function(name, alerted) {
 	if(StructuresDB.structures.hasOwnProperty(name))
@@ -318,7 +526,12 @@ Structure.init = function(folderName){
 		
 	StructuresDB.dir = dir;
 }
-
+Structure.setInWorld = function(name, x, y, z, rotate, progressively, time){
+	Structure.get(name).set(x, y, z, rotate, progressively, time);
+}
+Structure.destroyInWorld = function(name, x, y, z, rotate, progressively, time){
+	Structure.get(name).destroy(x, y, z, rotate, progressively, time);
+}
 
 /* OTHER FUNCTIONS */
 function rand(min, max){
@@ -328,7 +541,7 @@ function rand(min, max){
 	return (max-min) * Math.random() + min;
 }
 
-function getBlockSID(ID){
+function getSID(ID){
 	return IDRegistry.getNameByID(ID) || ID;
 }
 
@@ -360,7 +573,7 @@ if(JSON.parse(FileTools.ReadText(__dir__+"build.config")).defaultConfig.buildTyp
 			selectedArea:true,
 			event:function(args){
 				var pos = WorldEdit.getPosition();
-				let chest = 0;
+				let chest = 0, te = 0;
 				
 				let struct = Structure.get(args[0], false);
 				struct.clear();
@@ -375,18 +588,24 @@ if(JSON.parse(FileTools.ReadText(__dir__+"build.config")).defaultConfig.buildTyp
 							var block = World.getBlock(x,y,z);
 							if(args.indexOf("-a") == -1 && block.id == 0) continue;
 							
+							let TE = World.getTileEntity(x,y,z);
+							
 							if([54, 61, 62, 154].indexOf(block.id) != -1){
-								struct.addBlock(x - center_x, y - center_y, z - center_z, block, "chest_"+chest);
+								struct.addBlock(x - center_x, y - center_y, z - center_z, block, "chest_" + chest);
 								struct.addChest("chest_"+chest, World.getContainer(x, y, z));
 								chest++;
-							} else {
+							} else if(TE){
+								struct.addBlock(x - center_x, y - center_y, z - center_z, block, "TE_" + te);
+								struct.addTileEntity("TE_" + te, TE);
+								te++;
+							}else{
 								struct.addBlock(x - center_x, y - center_y, z - center_z, block);
 							}
 
 						}
 				
 				struct.save();
-				Game.message(Translation.sprintf("Saved to %s", StructuresAPI.dir+"/"+args[0]+".struct"));
+				Game.message(Translation.sprintf("Saved to %s", StructuresDB.dir+"/"+args[0]+".struct"));
 			}
 		});
 	});
@@ -422,138 +641,4 @@ if(!Translation.sprintf){
 }
 
 EXPORT("Structure", Structure);
-
-/* OUTDATED */
-var StructuresAPI = {
-	
-	dir:"structures",
-	structures:{},
-	
-	init:function(dir){
-		if(typeof dir != "string" && !(dir instanceof java.lang.String))
-			throw "dir is not string.";
-		
-		this.dir = dir;
-	},
-	
-	get:function(name){
-		if(!this.structures.hasOwnProperty(name)){
-			var path = __dir__ + this.dir + "/" + name + ".struct";
-			
-			if(FileTools.isExists(path)){
-				this.structures[name] = JSON.parse(FileTools.ReadText(path));
-			}else{
-				alert(Translation.sprintf("Structure \"%s\" not found.", name));
-				return false;
-			}
-		}
-		return this.structures[name];
-	},
-	
-	getStructures:function(name, x, y, z, rotate_y){
-		if(rotate_y === undefined) rotate_y = false;
-		var rotates = rotate_y == true ? StructuresAPI.ROTATE_Y : StructuresAPI.ROTATE_ALL;
-		
-		var struct = this.get(name);
-		if(struct == false) return;
-		
-		for(var i = 0; i < rotates.length; i++){
-			var rotate = rotates[i];
-			for(var k = 0; k < struct.length; k++){
-				var block = struct[k], id, data =0;
-				var dx = block[0] * rotate[0] + block[1] * rotate[1] + block[2] * rotate[2];
-				var dy = block[0] * rotate[3] + block[1] * rotate[4] + block[2] * rotate[5];
-				var dz = block[0] * rotate[6] + block[1] * rotate[7] + block[2] * rotate[8];
-				
-				if(typeof block[3] == "number")
-					id = block[3];
-				else if(typeof block[3] == "string")
-					id = BlockID[block[3]];
-				else{
-					id = block[3].id;
-					data = block[3].data;
-				}
-					
-				_block = World.getBlock(x + dx, y+dy, z+dz);
-				if(_block.id != id || _block.data != data) break;
-				
-				if(_block.id == id && _block.data == data && k == struct.length-1) return true;
-				
-			}
-		}
-		
-		return false;
-	},
-	
-	save:function(name, obj){
-		if(!FileTools.isExists(__dir__ + this.dir))
-			FileTools.mkdir(__dir__ + this.dir);
-		
-		for(var i = 0; i < obj.length; i++){
-			obj[i][3].id = getBlockSID(obj[i][3].id);
-			
-			if(obj[i][3].data == 0)
-				obj[i][3] = obj[i][3].id;
-		}
-		
-		this.structures[name] = obj;
-		FileTools.WriteText(__dir__ + this.dir + "/" + name + ".struct", JSON.stringify(obj));
-	},
-	
-	set: function (name, x, y, z,rotate, destroy, progressively, time) {
-		if (rotate === undefined) rotate = StructuresAPI.ROTATE_NONE;
-
-		if (rotate[0] instanceof Array) {
-			if (rotate.indexOf(StructuresAPI.ROTATE_NONE) == -1)
-				rotate.push(StructuresAPI.ROTATE_NONE);
-
-			rotate = rotate[Math.round(rand(0, rotate.length - 1))];
-		}
-
-		var arr = this.get(name);
-
-		if (arr === false) return false;
-		new java.lang.Thread(function () {
-			for (var i = 0; i < arr.length; i++) {
-				var block = arr[i];
-				var id, data = 0;
-
-				if (typeof block[3] == "number") {
-					id = block[3];
-				} else {
-					id = block[3].id || 2;
-					data = block[3].data || block[3].meta || 0;
-				}
-
-				var dx = block[0] * rotate[0] + block[1] * rotate[1] + block[2] * rotate[2];
-				var dy = block[0] * rotate[3] + block[1] * rotate[4] + block[2] * rotate[5];
-				var dz = block[0] * rotate[6] + block[1] * rotate[7] + block[2] * rotate[8];
-
-				if (!destroy) World.setBlock(x + dx, y + dy, z + dz, id, data)
-				else World.setBlock(x + dx, y + dy, z + dz, 0, 0)
-				if(progressively)java.lang.Thread.sleep(time || 100);
-			}
-		}).start();
-	}
-}
-
-StructuresAPI.ROTATE_RANDOM = [
-	StructuresAPI.ROTATE_180X,
-	StructuresAPI.ROTATE_180Y,
-	StructuresAPI.ROTATE_180Z,
-	StructuresAPI.ROTATE_270X,
-	StructuresAPI.ROTATE_270Y,
-	StructuresAPI.ROTATE_270Z,
-	StructuresAPI.ROTATE_90X,
-	StructuresAPI.ROTATE_90Y,
-	StructuresAPI.ROTATE_90Z,
-	StructuresAPI.ROTATE_NONE
-];
-StructuresAPI.ROTATE_ALL = StructuresAPI.ROTATE_RANDOM;
-StructuresAPI.ROTATE_Y = [
-	StructuresAPI.ROTATE_90Y,
-	StructuresAPI.ROTATE_270Y,
-	StructuresAPI.ROTATE_180Y,
-	StructuresAPI.ROTATE_NONE
-];
-EXPORT("StructuresAPI", StructuresAPI);
+EXPORT("Rotate", Rotate);
